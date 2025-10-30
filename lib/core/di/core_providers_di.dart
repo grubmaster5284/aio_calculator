6b00f754efd85932d61f25edde6f51ecacfa4da3
Satvik Gedam <rickysatvik524@gmail.com>
2025-10-30 20:26:42 +0530

    ci: build all platforms and use GH_PAT for release upload
    # Conflicts:
    #	.github/workflows/release.yml

--- lib/core/di/core_providers_di.dart
+++
@@ -1,8 +0,0 @@
-/// Main dependency injection container
-/// This file serves as the central point for all DI configurations
-/// and provides a clean separation between data layer and application layer
-library;
-
-export 'calculator_di.dart';
-export 'graphing_di.dart';
-
